208d49eeb8cf0de6f9a6e9b7b4329e14cd68b3ed
SpartanPlane <68822863+SpartanPlane@users.noreply.github.com>
2020-07-26 22:28:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,19 @@ bot = commands.Bot(command_prefix='!')
 async def test(ctx, arg): #создаем асинхронную фунцию бота
     await ctx.send(arg) #отправляем обратно аргумент
 
-bot.run(TOKEN) #Алексей не гей, он покермен Боооооооой
+bot.run(TOKEN) #Алексей не гей, он покермен Боооооооой  
 
+import discord
+from discord.ext import commands
+
+TOKEN = 'NzM3MDE2MTQ5NTU2MDY4NDAy.Xx3Nxw.7ufaMQaIDNbSl8EMRb5u6PFTKQw'
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(pass_context=True) #разрешаем передавать агрументы
+async def test(ctx, arg): #создаем асинхронную фунцию бота
+    await ctx.send(arg) #отправляем обратно аргумент
+
+bot.run(TOKEN) #Алексей не гей, он покермен Боооооооой  
 
 
 
